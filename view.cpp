e2d09f15e6b0e41d37cc0c62a96f89beb2ba5dc5
Anders Bakken <anders.bakken@nokia.com>
2010-03-02 20:33:32 -0800

    more work on dialog

--- view.cpp
+++ view.cpp
@@ -65,17 +65,62 @@ class TextEdit : public QTextEdit
 {
     Q_OBJECT
 public:
-    TextEdit()
+    TextEdit() : empty(true)
     {
         setEnabled(false);
         setTabChangesFocus(true);
+        connect(this, SIGNAL(textChanged()), this, SLOT(onTextChanged()));
     }
 
+    void keyPressEvent(QKeyEvent *e)
+    {
+        if (e->text().contains("|")) {
+            e->accept();
+        } else {
+            QTextEdit::keyPressEvent(e);
+        }
+    }
+
+    void paintEvent(QPaintEvent *e)
+    {
+        if (empty) {
+            QFont font;
+            font.setPixelSize(20);
+            QRect r = QFontMetrics(font).boundingRect(objectName());
+            r.moveCenter(rect().center());
+            if (e->rect().intersects(r)) {
+                QPainter p(viewport());
+                p.setPen(palette().text().color());
+                p.drawText(r, Qt::AlignCenter, objectName());
+            }
+        }
+        QTextEdit::paintEvent(e);
+    }
 public slots:
+    void onTextChanged()
+    {
+        const QString pipe = QLatin1String("|");
+        QTextCursor cursor(document());
+        const bool old = document()->blockSignals(true);
+        forever {
+            cursor = document()->find(pipe, cursor);
+            if (cursor.isNull())
+                break;
+            cursor.removeSelectedText();
+        }
+        document()->blockSignals(old);
+        if (empty != document()->isEmpty()) {
+            update();
+            empty = !empty;
+        }
+    }
+
     void onCategoryEditTextChanged(const QString &text)
     {
         setEnabled(!text.isEmpty());
     }
+private:
+    bool empty;
 };
 
 class GameDialog : public QDialog
@@ -93,7 +138,7 @@ public:
         connect(d.name, SIGNAL(textChanged(QString)), this, SLOT(updateOk()));
         layout->addWidget(d.name, 0, 1, 1, 5);
 
-        for (int i=0; i<Rows; ++i) {
+        for (int i=0; i<=Rows; ++i) {
             if (i == 0) {
                 layout->addWidget(new QLabel(tr("Categories")), 1, 0, 2, 1);
             } else {
@@ -108,54 +153,100 @@ public:
                     layout->addWidget(edit, i + 1, j + 1);
                 } else {
                     TextEdit *edit = new TextEdit;
-                    static const QLatin1String questionName("question");
+                    static const QLatin1String questionName("Question");
                     edit->setObjectName(questionName);
-                    d.edits[i - 1][j][Question] = edit;
+                    d.edits[j][i - 1][Question] = edit;
                     connect(edit, SIGNAL(textChanged()), this, SLOT(updateOk()));
                     connect(d.categories[j], SIGNAL(textChanged(QString)), edit, SLOT(onCategoryEditTextChanged(QString)));
                     layout->addWidget(edit, (i + 1) * 2, j + 1);
                     edit = new TextEdit;
-                    static const QLatin1String answerName("answer");
+                    static const QLatin1String answerName("Answer");
                     edit->setObjectName(answerName);
-                    d.edits[i - 1][j][Answer] = edit;
+                    d.edits[j][i - 1][Answer] = edit;
                     connect(edit, SIGNAL(textChanged()), this, SLOT(updateOk()));
                     connect(d.categories[j], SIGNAL(textChanged(QString)), edit, SLOT(onCategoryEditTextChanged(QString)));
                     layout->addWidget(edit, ((i + 1) * 2) + 1, j + 1);
                 }
             }
         }
-        setStyleSheet("TextEdit:disabled,QLineEdit:disabled { background: darkGray; }"
-                      "TextEdit#answer { text: white; background: black; }"
-                      "TextEdit#answer:disabled { text: white; background: darkGray; }");
+        setStyleSheet("QLineEdit:disabled { background: darkGray; }"
+                      "TextEdit#Question:disabled { background: darkGray; color: white; }"
+                      "TextEdit#Answer { color: white; background: black; }"
+                      "TextEdit#Answer:disabled { color: white; background: darkGray; }");
 
         d.buttonBox = new QDialogButtonBox(QDialogButtonBox::Save|QDialogButtonBox::Cancel, Qt::Horizontal, this);
         connect(d.buttonBox, SIGNAL(accepted()), this, SLOT(accept()));
         connect(d.buttonBox, SIGNAL(rejected()), this, SLOT(reject()));
         layout->addWidget(d.buttonBox, layout->rowCount(), 0, 1, layout->columnCount());
         d.buttonBox->button(QDialogButtonBox::Save)->setEnabled(false);
+    }
 
+    void accept()
+    {
+        QString name = d.name->text() + ".txt";
+        if (QFile::exists(name)) {
+            name.insert(name.size() - 4, "_%1");
+            int idx = 1;
+            while (QFile::exists(name.arg(idx)))
+                ++idx;
+            name = name.arg(idx);
+        }
+        QFile file(name);
+        file.open(QIODevice::WriteOnly);
+        save(&file);
+        QDialog::accept();
+    }
+
+    void save(QIODevice *device)
+    {
+        QTextStream ts(device);
+        for (int i=0; i<Columns; ++i) {
+            Q_ASSERT(isValid(i));
+            ts << d.categories[i]->text() << endl;
+            for (int j=0; j<Rows; ++j) {
+                ts << d.edits[i][j][Question]->toPlainText().replace(QLatin1Char('\n'), QLatin1Char(' ')).simplified()
+                   << QLatin1Char('|')
+                   << d.edits[i][j][Answer]->toPlainText().replace(QLatin1Char('\n'), QLatin1Char(' ')).simplified()
+                   << endl;
+            }
+            if (!isValid(i + 1))
+                break;
+            if (i + 1 < Columns)
+                ts << endl;
+        }
+    }
+
+    bool isValid(int column, bool *isEmpty = 0) const
+    {
+        if (d.categories[column]->text().isEmpty()) {
+            if (isEmpty)
+                *isEmpty = true;
+            return false;
+        }
+        if (isEmpty)
+            *isEmpty = false;
+        for (int i=0; i<Rows; ++i) {
+            if (d.edits[column][i][Question]->document()->isEmpty()
+                || d.edits[column][i][Answer]->document()->isEmpty())
+                return false;
+        }
+        return true;
     }
 public slots:
     void updateOk()
     {
-        bool hasCategory = false;
         const bool hasName = !d.name->text().isEmpty();
+        bool hasCategory = false;
+        bool invalid = false;
         if (hasName) {
             for (int i=0; i<Columns; ++i) {
-                bool found = false;
-                for (int j=0; j<Rows; ++j) {
-#warning gotta do stuff here
-//                     const bool empty = d.edits[j][i]->text().isEmpty();
-//                     if (found && empty) {
-//                         d.buttonBox->button(QDialogButtonBox::Save)->setEnabled(false);
-//                         return;
-//                     } else if (!empty && !found && j > 0) {
-//                         d.buttonBox->button(QDialogButtonBox::Save)->setEnabled(false);
-//                         return;
-//                     } else if (!empty) {
-//                         found = true;
-//                     }
-
+                bool empty;
+                if (isValid(i, &empty)) {
+                    hasCategory = true;
+                } else {
+                    if (!empty)
+                        invalid = true;
+                    break;
                 }
             }
         }
@@ -167,7 +258,7 @@ public slots:
             }
         }
 
-        d.buttonBox->button(QDialogButtonBox::Save)->setEnabled(hasCategory);
+        d.buttonBox->button(QDialogButtonBox::Save)->setEnabled(hasCategory && !invalid);
     }
 private:
     enum { Rows = 5, Columns = 5, Question = 0, Answer = 1 };
@@ -175,7 +266,7 @@ private:
         QLineEdit *name;
         QDialogButtonBox *buttonBox;
         QLineEdit *categories[Columns];
-        TextEdit *edits[Rows][Columns][2];
+        TextEdit *edits[Columns][Rows][2];
     } d;
 };
 
